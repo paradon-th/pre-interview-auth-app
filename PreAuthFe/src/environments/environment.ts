561f74402b8de676af00ecaf6d4826a8ef4add61
Paradon Thonthong(Burger) <paradon.thth@gmail.com>
2025-10-19 18:05:37 +0700

    edit env

--- PreAuthFe/src/environments/environment.ts
+++ PreAuthFe/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5058/api'
-    // apiUrl: 'http://localhost:8080/api'
+    // apiUrl: 'http://localhost:5058/api'
+    apiUrl: 'http://localhost:8080/api'
 
 };
